1b03155206462f6a5567906c0c34e2f0c34f1e3c
José de Senna <josespnt1990@gmail.com>
2026-02-10 13:47:18 -0300

    feat: solicita entradas de console para atributos e operacoes

--- ControleEstoqueBasico/Program.cs
+++ ControleEstoqueBasico/Program.cs
@@ -8,22 +8,29 @@ class Program
     static void Main(string[] args)
     {
         Produto produto = new Produto();
-        produto.Nome = "Exemplo";
-        produto.Preco = 10.00;
-        produto.Quantidade = 0;
+        Console.WriteLine("Entre com os dados do produto:");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine()!;
+        Console.Write("Preço: ");
+        produto.Preco = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Console.Write("Quantidade em estoque: ");
+        produto.Quantidade = int.Parse(Console.ReadLine()!);
 
+        Console.WriteLine("");
         Console.Write("Dados do produto: ");
         ExibirDados(produto);
 
         Console.WriteLine("");
-        Console.WriteLine("Adicionando 5 produtos...");
-        produto.AdicionarProdutos(5);
+        Console.Write("Digite o número de unidades a ser adicionada ao estoque: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
+        produto.AdicionarProdutos(quantidade);
         Console.Write("Dados atualizados: ");
         ExibirDados(produto);
 
         Console.WriteLine("");
-        Console.WriteLine("Removendo 2 produtos...");
-        produto.RemoverProdutos(2);
+        Console.Write("Digite o número de unidades a ser removida do estoque: ");
+        quantidade = int.Parse(Console.ReadLine()!);
+        produto.RemoverProdutos(quantidade);
         Console.Write("Dados atualizados: ");
         ExibirDados(produto);
     }
