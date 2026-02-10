1780ca8b67b427f63e15b8c623a7a627a568da49
José de Senna <josespnt1990@gmail.com>
2026-02-10 12:31:52 -0300

    feat: cria logica de uso da classe produto
    - Inclui funcao para exibir os dados do produto

--- ControleEstoqueBasico/Program.cs
+++ ControleEstoqueBasico/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace ControleEstoqueBasico;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+        produto.Nome = "Exemplo";
+        produto.Preco = 10.00;
+        produto.Quantidade = 0;
+
+        ExibirDados(produto);
+
+        Console.WriteLine("");
+        Console.WriteLine("Adicionando 5 produtos...");
+        produto.AdicionarProdutos(5);
+        ExibirDados(produto);
+
+        Console.WriteLine("");
+        Console.WriteLine("Removendo 2 produtos...");
+        produto.RemoverProdutos(2);
+        ExibirDados(produto);
+    }
+
+    static void ExibirDados(Produto produto)
+    {
+        Console.WriteLine("Nome do produto: " + produto.Nome + ".");
+        Console.WriteLine("Preço do produto: R$ " + produto.Preco.ToString("F2", CultureInfo.InvariantCulture) + ".");
+        Console.WriteLine("Quantidade em estoque: " + produto.Quantidade + " unidade(s).");
+        Console.WriteLine("Valor do Estoque: R$ " + produto.ValorEstoque().ToString("F2", CultureInfo.InvariantCulture) + ".");
+    }
+}
