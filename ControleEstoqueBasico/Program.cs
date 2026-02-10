88439b6ba4036e130dc68c0ebc1c9f1f48c91b66
José de Senna <josespnt1990@gmail.com>
2026-02-10 13:36:32 -0300

    refactor(ExibirDados): atualiza formado de exibicao dos dados
    - Faz os dados do produto serem exibidos em uma unica linha

--- ControleEstoqueBasico/Program.cs
+++ ControleEstoqueBasico/Program.cs
@@ -12,24 +12,27 @@ class Program
         produto.Preco = 10.00;
         produto.Quantidade = 0;
 
+        Console.Write("Dados do produto: ");
         ExibirDados(produto);
 
         Console.WriteLine("");
         Console.WriteLine("Adicionando 5 produtos...");
         produto.AdicionarProdutos(5);
+        Console.Write("Dados atualizados: ");
         ExibirDados(produto);
 
         Console.WriteLine("");
         Console.WriteLine("Removendo 2 produtos...");
         produto.RemoverProdutos(2);
+        Console.Write("Dados atualizados: ");
         ExibirDados(produto);
     }
 
     static void ExibirDados(Produto produto)
     {
-        Console.WriteLine("Nome do produto: " + produto.Nome + ".");
-        Console.WriteLine("Preço do produto: R$ " + produto.Preco.ToString("F2", CultureInfo.InvariantCulture) + ".");
-        Console.WriteLine("Quantidade em estoque: " + produto.Quantidade + " unidade(s).");
-        Console.WriteLine("Valor do Estoque: R$ " + produto.ValorEstoque().ToString("F2", CultureInfo.InvariantCulture) + ".");
+        Console.Write(produto.Nome + ", R$ ");
+        Console.Write(produto.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " );
+        Console.Write(produto.Quantidade + " unidade(s), Valor em estoque: R$ ");
+        Console.WriteLine(produto.ValorEstoque().ToString("F2", CultureInfo.InvariantCulture) + ".");
     }
 }
