ba611cadd630be9d20b97cb10b724081da3a2692
José de Senna <josespnt1990@gmail.com>
2026-02-11 18:39:55 -0300

    refactor: substitui getters/setters por properties

--- ControleEstoqueBasico/Produto.cs
+++ ControleEstoqueBasico/Produto.cs
@@ -21,27 +21,36 @@ class Produto
         _quantidade = quantidade;
     }
 
-    public string GetNome()
+    public string Nome
     {
-        return _nome;
-    }
-
-    public void SetNome(string nome)
-    {
-        if (nome != null && nome.Length > 1)
+        get
         {
-            _nome = nome;
+            return _nome;
+        }
+
+        set
+        {
+            if (value != null && value.Length > 1)
+            {
+                _nome = value;
+            }
         }
     }
 
-    public double GetPreco()
+    public double Preco
     {
-        return _preco;
+        get
+        {
+            return _preco;
+        }
     }
 
-    public int GetQuantidade()
+    public int Quantidade
     {
-        return _quantidade;
+        get
+        {
+            return _quantidade;
+        }
     }
 
     public double ValorEstoque()
