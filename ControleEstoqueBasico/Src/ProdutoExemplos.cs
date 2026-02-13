53c4370088857944b5b3597e09f786642a8fb3a3
José de Senna <josespnt1990@gmail.com>
2026-02-13 13:02:35 -0300

    feat: adiciona exemplo de uso de construtores

--- ControleEstoqueBasico/Src/ProdutoExemplos.cs
+++ ControleEstoqueBasico/Src/ProdutoExemplos.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace ControleEstoqueBasico.Src;
 
@@ -16,5 +17,27 @@ static class ProdutoExemplos
         Console.WriteLine();
         ProdutoAuxiliares.RemoverUnidades(produto);
         Console.WriteLine(produto.ToString());
+    }
+
+    static public void ExemploConstrutores()
+    {
+        Console.WriteLine("Exemplo de uso de construtor completo:");
+        Console.WriteLine("Digite o nome: ");
+        string nome = Console.ReadLine()!;
+        Console.WriteLine("Digite o preço: ");
+        double preco = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite a quantidade em estoque: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
+        Produto produto1 = new Produto(nome, preco, quantidade);
+        Console.WriteLine(produto1.ToString());
+
+        Console.WriteLine();
+        Console.WriteLine("Exemplo de uso com construtor parcial (sobrecarga): ");
+        Console.WriteLine("Digite o nome: ");
+        nome = Console.ReadLine()!;
+        Console.WriteLine("Digite o preço: ");
+        preco = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Produto produto2 = new Produto(nome, preco);
+        Console.WriteLine(produto2.ToString());
     }
 }
